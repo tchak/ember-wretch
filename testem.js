360d50cd308173e41a6910e74023e73628e107d9
Paul Chavard <git@paul.chavard.net>
2017-10-09 13:33:36 +0300

    configure firefox tests

--- testem.js
+++ testem.js
@@ -2,13 +2,10 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
+  launch_in_ci: ['Chrome', 'Firefox'],
+  launch_in_dev: ['Chrome', 'Firefox'],
   browser_args: {
+    Firefox: ['-headless'],
     Chrome: [
       '--disable-gpu',
       '--headless',

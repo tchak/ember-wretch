9961914bdef620bfbde54bc589058ab8327fef28
Paul Chavard <git@paul.chavard.net>
2018-03-25 21:15:02 +0200

    Update ember-cli (3.0.2)

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-/* eslint-env node */
 'use strict';
 
 module.exports = {
   name: 'ember-wretch',
-
-  options: {
-    nodeAssets: {
-      wretch: {
-        vendor: {
-          include: ['dist/bundle/wretch.js']
-        }
-      }
-    }
-  },
-
   included: function() {
     this._super.included.apply(this, arguments);
 
-    this.import('vendor/wretch/dist/bundle/wretch.js', {
+    this.import('node_modules/wretch/dist/bundle/wretch.js', {
       using: [{ transformation: 'amd', as: 'wretch' }]
     });
   }
